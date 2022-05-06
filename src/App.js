0d7a3ae63834442ed26f5f959df1943cb498b90b
abhishekpathak232 <88376838+abhishekpathak232@users.noreply.github.com>
2022-05-06 16:22:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Devops
+          Hi Abhishek Welcome to React Learning Session
         </a>
       </header>
     </div>
